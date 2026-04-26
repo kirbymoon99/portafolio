034d36155803b6235fe288a9b286f3fea72a2d90
Irving Rodriguez <irvingroda9@gmail.com>
2026-04-25 22:09:56 -0600

    skills-final

--- script.js
+++ script.js
@@ -1,15 +1,239 @@
 const skills = {
-  arduino: {
-    nombre: "IDE Arduino",
-    nivel: 75,
-    logo: "img/skills/arduino.png",
-    comentario: "Experiencia programando microcontroladores y proyectos IoT."
+  vscode: {
+    name: "VSCode",
+    level: 75,
+    logo: "img/skills/vsc.png",
+    comment: "Mi editor de codigo por excelencia."
   },
-  motores: {
-    nombre: "Motores Eléctricos",
-    nivel: 60,
-    logo: "img/skills/motor.png"
-  }
+  c: {
+    name: "C",
+    level: 80,
+    logo: "img/skills/c.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  cpp: {
+    name: "C++",
+    level: 63,
+    logo: "img/skills/c++.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  python: {
+    name: "Python",
+    level: 55,
+    logo: "img/skills/python.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  csharp: {
+    name: "C#",
+    level: 40,
+    logo: "img/skills/csharp.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  javaScript: {
+    name: "JavaScript",
+    level: 65,
+    logo: "img/skills/js.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  java: {
+    name: "Java",
+    level: 33,
+    logo: "img/skills/java.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  mathLab: {
+    name: "Mathlab",
+    level: 45,
+    logo: "img/skills/mathlab.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  assembler: {
+    name: "Ensamblador",
+    level: 80,
+    logo: "img/skills/ams.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  html: {
+    name: "HTML",
+    level: 87,
+    logo: "img/skills/html.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  css: {
+    name: "CSS",
+    level: 89,
+    logo: "img/skills/css.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  node: {
+    name: "Node.js",
+    level: 59,
+    logo: "img/skills/node.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  net: {
+    name: ".NET",
+    level: 35,
+    logo: "img/skills/net.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  react: {
+    name: "React",
+    level: 32,
+    logo: "img/skills/react.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  sql: {
+    name: "SQL Server",
+    level: 80,
+    logo: "img/skills/sqlserver.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  mysql: {
+    name: "MySQL",
+    level: 56,
+    logo: "img/skills/mysql.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  oracle: {
+    name: "Oracle",
+    level: 41,
+    logo: "img/skills/oracle.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  access: {
+    name: "Access",
+    level: 42,
+    logo: "img/skills/access.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  git: {
+    name: "Git",
+    level: 50,
+    logo: "img/skills/git.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  github: {
+    name: "Github",
+    level: 57,
+    logo: "img/skills/github.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  unity: {
+    name: "Unity",
+    level: 61,
+    logo: "img/skills/unity.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  blender: {
+    name: "Blender",
+    level: 53,
+    logo: "img/skills/blender.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  plc: {
+    name: "PLC",
+    level: 84,
+    logo: "img/skills/plc.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  step7: {
+    name: "STEP 7",
+    level: 64,
+    logo: "img/skills/step7.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  tiap: {
+    name: "TIA Portal",
+    level: 48,
+    logo: "img/skills/tia.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  arduino: {
+    name: "IDE Arduino",
+    level: 87,
+    logo: "img/skills/arduino.png",
+    comment: "Experiencia programando microcontroladores y proyectos IoT."
+  },
+  motor: {
+    name: "Motores Eléctricos",
+    level: 60,
+    logo: "img/skills/motor.png",
+    comment: "Automatizacion de motores."
+  },
+  neumaticahidrahulica: {
+    name: "Neumatica e Hidrahulica",
+    level: 72,
+    logo: "img/skills/neumatica.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  electronica: {
+    name: "Electronica",
+    level: 81,
+    logo: "img/skills/electronica.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  electrica: {
+    name: "Electrica",
+    level: 58,
+    logo: "img/skills/electrica.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  powerbi: {
+    name: "Power BI",
+    level: 39,
+    logo: "img/skills/powerbi.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  solidworks: {
+    name: "SolidWorks",
+    level: 82,
+    logo: "img/skills/solidworks.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  fusion: {
+    name: "Autodek Fusion",
+    level: 89,
+    logo: "img/skills/fusion.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  autoCAD: {
+    name: "autoCAD",
+    level: 46,
+    logo: "img/skills/autoCAD.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  multisim: {
+    name: "Multisim",
+    level: 65,
+    logo: "img/skills/multisim.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  proteus: {
+    name: "Proteus",
+    level: 57,
+    logo: "img/skills/proteus.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  office: {
+    name: "Paqueteria Office",
+    level: 85,
+    logo: "img/skills/office.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  linux: {
+    name: "Linux",
+    level: 56,
+    logo: "img/skills/linux.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+  bash: {
+    name: "Bash",
+    level: 47,
+    logo: "img/skills/bash.png",
+    comment: "Mi editor de codigo por excelencia."
+  },
+
 };
 
 const botones = document.querySelectorAll(".skill-li");
@@ -28,20 +252,25 @@ function mostrarSkill(skill) {
 
 contenedor.innerHTML = `
   <div class="skill-container blink2">
-    <img src="${data.logo}" class="logo">
     
-    <div class="barra">
-      <div class="nivel"></div>
-    </div>
+  <h2 class="font-screen letter">¡Elije la habilidad!</h2>
+    
     <br>
+    <img src="${data.logo}" class="logo-screen">
 
-    <p class="nombre letter">${data.nombre}</p>
-    <p class="comentario letter">${data.comentario}</p>
+    <p class="nombre letter">${data.name}</p>
+
+    <div class="bar">
+      <div class="level"></div>
+    </div>
+
+    <p class="letter let-screen">Nivel: ${data.level}/100</p>
+    <p class="skill-grid letter let-screen">${data.comment}</p>
   </div>
 `;
 
   // Animación tipo "llenado"
   setTimeout(() => {
-    contenedor.querySelector(".nivel").style.width = data.nivel + "%";
+    contenedor.querySelector(".level").style.width = data.level + "%";
   }, 100);
 }

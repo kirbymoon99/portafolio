03fe0244a4c48cd8e76a34a62f21aa6f4e4ba8d8
Irving Rodriguez <irvingroda9@gmail.com>
2026-04-27 19:50:45 -0600

    final-skills

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+document.querySelectorAll(".game").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const id = btn.dataset.modal;
+    const modal = document.getElementById(id);
+    modal.showModal();
+  });
+});
+
+
+document.querySelectorAll(".cerrar").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.closest("dialog").close();
+  });
+});
+
 const skills = {
   vscode: {
     name: "VSCode",
@@ -9,217 +24,217 @@ const skills = {
     name: "C",
     level: 80,
     logo: "img/skills/c.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Mi primer acercamiento a la programacion y el primer lengueje que aprendi."
   },
   cpp: {
     name: "C++",
     level: 63,
     logo: "img/skills/c++.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "El primer lenjuage para la programacion orientada a objetos ademas de ser la base para las buenas practica de un programador."
   },
   python: {
     name: "Python",
     level: 55,
     logo: "img/skills/python.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Manejo de lenguaje en un entorno mas de microcontroladores que para desarrollo web. <br> Manejo de librerias como pandas"
   },
   csharp: {
     name: "C#",
     level: 40,
     logo: "img/skills/csharp.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Lengueje para el desarrollo de software y para .NET. <br> Creacion de web API'S"
   },
   javaScript: {
     name: "JavaScript",
     level: 65,
     logo: "img/skills/js.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Conocimiento en manipulacion del DOM y fetch para consumo de API'S."
   },
   java: {
     name: "Java",
     level: 33,
     logo: "img/skills/java.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Un lenguaje que recientemente estoy aprendiendo."
   },
   mathLab: {
     name: "Mathlab",
     level: 45,
     logo: "img/skills/mathlab.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Codigo para analisis de datos y programacion de robots conplejos ademas de control PID. <br> Lo aprendi para un barazo robotico que necesitaba polinomios de bezier."
   },
   assembler: {
     name: "Ensamblador",
     level: 80,
     logo: "img/skills/ams.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "El lenguaje para microprocesadores. <br> Manejo arquitectura VonNewman y Harvard ademas de intrucciones RISC Y CISC."
   },
   html: {
     name: "HTML",
     level: 87,
     logo: "img/skills/html.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Buen manejo de etiquetado de codigo."
   },
   css: {
     name: "CSS",
     level: 89,
     logo: "img/skills/css.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Dominio de flexbox y grid ademas de escalabilidad de codigo."
   },
   node: {
     name: "Node.js",
     level: 59,
     logo: "img/skills/node.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Mi backend preferido. <br> Conocimiento en Express para montaje de servidores y creacion de API'S RESTful en formato Json."
   },
   net: {
     name: ".NET",
     level: 35,
     logo: "img/skills/net.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Aprendiendo recientemente pero con base solida en C#."
   },
   react: {
     name: "React",
     level: 32,
     logo: "img/skills/react.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Aprendiendo recientemente."
   },
   sql: {
     name: "SQL Server",
     level: 80,
     logo: "img/skills/sqlserver.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia en consultas complejas, triggers, job etc."
   },
   mysql: {
     name: "MySQL",
     level: 56,
     logo: "img/skills/mysql.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia en consultas."
   },
   oracle: {
     name: "Oracle",
     level: 41,
     logo: "img/skills/oracle.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Manejo de datos complejos."
   },
   access: {
     name: "Access",
     level: 42,
     logo: "img/skills/access.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Creacion de formalario y bases de datos rapidas."
   },
   git: {
     name: "Git",
     level: 50,
     logo: "img/skills/git.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Manjeo de comandos escenciales y creacion de ramas."
   },
   github: {
     name: "Github",
     level: 57,
     logo: "img/skills/github.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "  Experiencia en gestion de proyectos colaborativos."
   },
   unity: {
     name: "Unity",
     level: 61,
     logo: "img/skills/unity.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia en la creacion de mapa tridimensional y programacion de agente."
   },
   blender: {
     name: "Blender",
     level: 53,
     logo: "img/skills/blender.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia en mapeo UV."
   },
   plc: {
     name: "PLC",
     level: 84,
     logo: "img/skills/plc.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Dominio de lenguaje ladder, FDB y SFC."
   },
   step7: {
     name: "STEP 7",
     level: 64,
     logo: "img/skills/step7.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia para la programacion de PLC Siemens."
   },
   tiap: {
     name: "TIA Portal",
     level: 48,
     logo: "img/skills/tia.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia para creacion de sistema SCADA."
   },
   arduino: {
     name: "IDE Arduino",
     level: 87,
     logo: "img/skills/arduino.png",
-    comment: "Experiencia programando microcontroladores y proyectos IoT."
+    comment: "Experiencia programando microcontroladores para la automatizacion de sistemas y de IoT."
   },
   motor: {
     name: "Motores Eléctricos",
     level: 60,
     logo: "img/skills/motor.png",
-    comment: "Automatizacion de motores."
+    comment: "Automatizacion de motores con variadores de frecuencia y PLC."
   },
   neumaticahidrahulica: {
     name: "Neumatica e Hidrahulica",
     level: 72,
     logo: "img/skills/neumatica.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Experiencia en secuencias y creacion de sistemas automatizados."
   },
   electronica: {
     name: "Electronica",
     level: 81,
     logo: "img/skills/electronica.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Creacion, mantenimiento y reparacion de placas de circuitos electronicos."
   },
   electrica: {
     name: "Electrica",
     level: 58,
     logo: "img/skills/electrica.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Manipulacion de baja tension ademas de tener experiencia en montaje de centros de carga."
   },
   powerbi: {
     name: "Power BI",
     level: 39,
     logo: "img/skills/powerbi.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Lo uso mas para presentar un analisis de datos formal."
   },
   solidworks: {
     name: "SolidWorks",
     level: 82,
     logo: "img/skills/solidworks.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Conocimiento para pruebas mecánicas."
   },
   fusion: {
     name: "Autodek Fusion",
     level: 89,
     logo: "img/skills/fusion.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Mi software de diseño mecánico preferido."
   },
   autoCAD: {
     name: "autoCAD",
     level: 46,
     logo: "img/skills/autoCAD.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Software para la creacion de planos eléctricos y de control."
   },
   multisim: {
     name: "Multisim",
     level: 65,
     logo: "img/skills/multisim.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Lo uso para pruebas electronicas."
   },
   proteus: {
     name: "Proteus",
     level: 57,
     logo: "img/skills/proteus.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Conocimiento en creacion de placas electronicas."
   },
   office: {
     name: "Paqueteria Office",
     level: 85,
     logo: "img/skills/office.png",
-    comment: "Mi editor de codigo por excelencia."
+    comment: "Manejo de todos los softwares de la paqueteria de office."
   },
   linux: {
     name: "Linux",
@@ -269,7 +284,6 @@ contenedor.innerHTML = `
   </div>
 `;
 
-  // Animación tipo "llenado"
   setTimeout(() => {
     contenedor.querySelector(".level").style.width = data.level + "%";
   }, 100);
